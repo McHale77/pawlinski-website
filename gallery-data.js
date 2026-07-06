4fa28a8380cc1533d82552cbcc6793ebde1686d4
McHale77 <mpc@life.pl>
2026-07-06 20:11:01 +0200

    Galeria portfolio: miniatury i pelne wersje, kompresja tel

---
+++ gallery-data.js
@@ -0,0 +1,861 @@
+// Dane galerii — wygenerowane automatycznie z tagów Lightroom
+// 119 zdjęć, 8 kategorii. Miniatury: images/thumbnails/<file>, pełne: images/full/<file>
+const GALLERY_DATA = {
+  "categories": [
+    {
+      "id": "eventy",
+      "label": "Eventy",
+      "count": 54
+    },
+    {
+      "id": "biznes",
+      "label": "Biznes",
+      "count": 37
+    },
+    {
+      "id": "ludzie",
+      "label": "Ludzie",
+      "count": 36
+    },
+    {
+      "id": "marka",
+      "label": "Marka i produkt",
+      "count": 26
+    },
+    {
+      "id": "hobby",
+      "label": "Hobby",
+      "count": 24
+    },
+    {
+      "id": "natura",
+      "label": "Natura",
+      "count": 15
+    },
+    {
+      "id": "celebracje",
+      "label": "Celebracje",
+      "count": 14
+    },
+    {
+      "id": "streetart",
+      "label": "Street art",
+      "count": 12
+    }
+  ],
+  "images": [
+    {
+      "file": "1-2.jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "1.jpg",
+      "cats": [
+        "biznes",
+        "eventy",
+        "marka"
+      ]
+    },
+    {
+      "file": "11-2-2.jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "11-2.jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "12.jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "13-2.jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "13.jpg",
+      "cats": [
+        "marka"
+      ]
+    },
+    {
+      "file": "14.jpg",
+      "cats": [
+        "biznes"
+      ]
+    },
+    {
+      "file": "16.jpg",
+      "cats": [
+        "biznes"
+      ]
+    },
+    {
+      "file": "17.jpg",
+      "cats": [
+        "biznes",
+        "eventy"
+      ]
+    },
+    {
+      "file": "18.jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "2-2.jpg",
+      "cats": [
+        "eventy",
+        "hobby",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "2-3.jpg",
+      "cats": [
+        "hobby",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "2.jpg",
+      "cats": [
+        "hobby",
+        "ludzie",
+        "streetart"
+      ]
+    },
+    {
+      "file": "21.jpg",
+      "cats": [
+        "biznes",
+        "eventy",
+        "marka"
+      ]
+    },
+    {
+      "file": "22.jpg",
+      "cats": [
+        "biznes",
+        "ludzie",
+        "marka"
+      ]
+    },
+    {
+      "file": "24.jpg",
+      "cats": [
+        "eventy",
+        "marka"
+      ]
+    },
+    {
+      "file": "25.jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "27.jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "3 (1)-2.jpg",
+      "cats": [
+        "eventy",
+        "hobby",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (1).jpg",
+      "cats": [
+        "eventy",
+        "marka",
+        "streetart"
+      ]
+    },
+    {
+      "file": "3 (10)-2.jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "3 (10)-3.jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (10).jpg",
+      "cats": [
+        "celebracje",
+        "eventy",
+        "streetart"
+      ]
+    },
+    {
+      "file": "3 (12)-2.jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (12)-3.jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (12).jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "3 (13)-2.jpg",
+      "cats": [
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (13)-3.jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (13).jpg",
+      "cats": [
+        "biznes",
+        "celebracje",
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (14)-2.jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (14)-3.jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (14).jpg",
+      "cats": [
+        "celebracje"
+      ]
+    },
+    {
+      "file": "3 (15)-2.jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (15).jpg",
+      "cats": [
+        "biznes",
+        "eventy",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (16)-2.jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (16).jpg",
+      "cats": [
+        "biznes"
+      ]
+    },
+    {
+      "file": "3 (17)-2.jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "3 (17).jpg",
+      "cats": [
+        "eventy",
+        "marka"
+      ]
+    },
+    {
+      "file": "3 (18)-2.jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (18)-3.jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (18).jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (19)-2.jpg",
+      "cats": [
+        "hobby",
+        "natura",
+        "streetart"
+      ]
+    },
+    {
+      "file": "3 (19).jpg",
+      "cats": [
+        "celebracje"
+      ]
+    },
+    {
+      "file": "3 (2).jpg",
+      "cats": [
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (20)-2.jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (20).jpg",
+      "cats": [
+        "biznes",
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (21).jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (22).jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "3 (23).jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (24)-2.jpg",
+      "cats": [
+        "hobby",
+        "streetart"
+      ]
+    },
+    {
+      "file": "3 (24).jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (25)-2.jpg",
+      "cats": [
+        "hobby",
+        "streetart"
+      ]
+    },
+    {
+      "file": "3 (25).jpg",
+      "cats": [
+        "celebracje",
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (27).jpg",
+      "cats": [
+        "biznes",
+        "eventy",
+        "marka"
+      ]
+    },
+    {
+      "file": "3 (29).jpg",
+      "cats": [
+        "celebracje",
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (3).jpg",
+      "cats": [
+        "celebracje"
+      ]
+    },
+    {
+      "file": "3 (30).jpg",
+      "cats": [
+        "biznes"
+      ]
+    },
+    {
+      "file": "3 (31).jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (32).jpg",
+      "cats": [
+        "biznes",
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (33).jpg",
+      "cats": [
+        "celebracje",
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (34).jpg",
+      "cats": [
+        "ludzie",
+        "streetart"
+      ]
+    },
+    {
+      "file": "3 (36).jpg",
+      "cats": [
+        "eventy",
+        "marka"
+      ]
+    },
+    {
+      "file": "3 (39).jpg",
+      "cats": [
+        "celebracje"
+      ]
+    },
+    {
+      "file": "3 (4)-2.jpg",
+      "cats": [
+        "ludzie",
+        "streetart"
+      ]
+    },
+    {
+      "file": "3 (4)-3.jpg",
+      "cats": [
+        "hobby",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (4)-4.jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (4).jpg",
+      "cats": [
+        "eventy",
+        "hobby"
+      ]
+    },
+    {
+      "file": "3 (40).jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (41).jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (42).jpg",
+      "cats": [
+        "biznes",
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (43).jpg",
+      "cats": [
+        "biznes",
+        "eventy",
+        "marka"
+      ]
+    },
+    {
+      "file": "3 (44).jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (45).jpg",
+      "cats": [
+        "celebracje"
+      ]
+    },
+    {
+      "file": "3 (47).jpg",
+      "cats": [
+        "biznes",
+        "ludzie",
+        "marka"
+      ]
+    },
+    {
+      "file": "3 (48).jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (49).jpg",
+      "cats": [
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (5)-2.jpg",
+      "cats": [
+        "hobby",
+        "streetart"
+      ]
+    },
+    {
+      "file": "3 (5)-3.jpg",
+      "cats": [
+        "celebracje",
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (5)-4.jpg",
+      "cats": [
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (5).jpg",
+      "cats": [
+        "biznes"
+      ]
+    },
+    {
+      "file": "3 (50).jpg",
+      "cats": [
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (51).jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (52).jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (53).jpg",
+      "cats": [
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (54).jpg",
+      "cats": [
+        "biznes",
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (55).jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (56).jpg",
+      "cats": [
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (58).jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (6).jpg",
+      "cats": [
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (60).jpg",
+      "cats": [
+        "celebracje",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (61).jpg",
+      "cats": [
+        "eventy",
+        "ludzie",
+        "streetart"
+      ]
+    },
+    {
+      "file": "3 (62).jpg",
+      "cats": [
+        "celebracje",
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (7)-2.jpg",
+      "cats": [
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (7)-3.jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (7)-4.jpg",
+      "cats": [
+        "hobby",
+        "natura",
+        "streetart"
+      ]
+    },
+    {
+      "file": "3 (7).jpg",
+      "cats": [
+        "biznes"
+      ]
+    },
+    {
+      "file": "3 (8)-2.jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (8)-3.jpg",
+      "cats": [
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (8)-4.jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (8).jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (9)-2.jpg",
+      "cats": [
+        "celebracje",
+        "eventy"
+      ]
+    },
+    {
+      "file": "3 (9)-3.jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3 (9)-4.jpg",
+      "cats": [
+        "hobby",
+        "natura"
+      ]
+    },
+    {
+      "file": "3 (9).jpg",
+      "cats": [
+        "biznes",
+        "eventy"
+      ]
+    },
+    {
+      "file": "3-2.jpg",
+      "cats": [
+        "eventy",
+        "ludzie"
+      ]
+    },
+    {
+      "file": "3.jpg",
+      "cats": [
+        "biznes",
+        "hobby",
+        "ludzie",
+        "natura"
+      ]
+    },
+    {
+      "file": "31.jpg",
+      "cats": [
+        "biznes"
+      ]
+    },
+    {
+      "file": "34.jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "37.jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "38.jpg",
+      "cats": [
+        "biznes"
+      ]
+    },
+    {
+      "file": "4.jpg",
+      "cats": [
+        "biznes",
+        "eventy",
+        "marka"
+      ]
+    },
+    {
+      "file": "40.jpg",
+      "cats": [
+        "biznes",
+        "eventy"
+      ]
+    },
+    {
+      "file": "5.jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "6-2.jpg",
+      "cats": [
+        "marka"
+      ]
+    },
+    {
+      "file": "6.jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "7-2.jpg",
+      "cats": [
+        "ludzie"
+      ]
+    },
+    {
+      "file": "7.jpg",
+      "cats": [
+        "biznes",
+        "marka"
+      ]
+    },
+    {
+      "file": "9.jpg",
+      "cats": [
+        "hobby",
+        "marka",
+        "streetart"
+      ]
+    }
+  ]
+};
